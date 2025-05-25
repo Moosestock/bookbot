712a8178b9ea4bfb7cb21f195b71eec0353e01d8
moosestock <83773849+Moosestock@users.noreply.github.com>
2025-05-25 16:05:35 -0400

    First BookBot assignment complete - haza!

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-def get_book_text(bookName):
-	with open(bookName) as f:
-		bookContents = f.read()
-		#print(bookContents)
-		numWords = bookContents.split()
-		print(str(len(numWords)) + " words found in the document")
-
 def main():
-	get_book_text("books/frankenstein.txt")
+	from stats import getBookText
+	bookText = getBookText("books/frankenstein.txt")
+
+	from stats import getBookWordCount
+	splitBookWords = getBookWordCount(bookText)
+
+	from stats import setBookWordsLowercase
+	lowerBookWords = setBookWordsLowercase(bookText)
+#	print(lowerBookWords)
+
+	from stats import countCharactersInBook
+	charCount = {}
+	charCount = countCharactersInBook(lowerBookWords)
+
+	from stats import sortCharacters
+	listOfChars = sortCharacters(charCount)
+#	print(len(listOfChars))
+#	print(listOfChars)
+
+#	print(type(listOfChars[0]))
+#	print(listOfChars[0])
+
+	from stats import printCharCount
+	printCharCount(listOfChars, len(splitBookWords))
 
 main()
---
+++ stats.py
@@ -0,0 +1,57 @@
+def getBookText(bookName):
+	with open(bookName) as f:
+		bookText = f.read()
+#		print(bookText)
+		return bookText
+
+def getBookWordCount(bookText):
+	bookWords = bookText.split()
+#	print(str(len(bookWords)) + " words found in the document")
+	return bookWords
+
+def setBookWordsLowercase(bookWords):
+	lowerBookWords = ()
+	lowerBookWords = bookWords.lower()
+	return lowerBookWords
+
+def countCharactersInBook(lowerBookWords):
+	charCount = {}
+#	print(lowerBookWords)
+	bookCharacters = list(lowerBookWords)
+	for character in bookCharacters:
+		if character in charCount:
+			charCount[character] += 1
+		else:
+			charCount[character] = 1
+	return charCount
+
+def sortOn(dict):
+	return dict["num"]
+
+def sortCharacters(charCount):
+	listOfChars = []
+	sortedChars = {}
+
+	for k, v in charCount.items():
+		if k.isalpha():
+			sortedChars = {"char": k, "num": v}
+			listOfChars.append(sortedChars)
+	listOfChars.sort(reverse = True, key=sortOn)
+
+	return listOfChars
+
+def printCharCount(listOfChars, wordCount):
+	print("============ BOOKBOT ============")
+	print("Analyzing book found at books/frankenstein.txt...")
+	print("----------- Word Count ----------")
+	print(f"Found {wordCount} total words")
+	print("--------- Character Count -------")
+
+	for charL in listOfChars:
+		outputChar = f"{charL['char']}: {charL['num']}" 
+		print(outputChar)
+
+#	print(len(charList))
+ # ": " charList[char]["num"])
+
+	print("============= END ===============")
